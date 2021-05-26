c922770a5be67610d701c47468f18db1446f0503
Nazmus-Shakib <shakibnazmus16@gmail.com>
2021-05-27 01:30:52 +0800

    Backend Completed

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,31 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
+// frontend routes
 Route::get('/', 'Frontend\FrontendController@index');
-Route::get('about-us', 'Frontend\FrontendController@aboutUs')->name('about.us');
-Route::get('contact-us', 'Frontend\FrontendController@contactUs')->name('contact.us');
+Route::get('/about-us', 'Frontend\FrontendController@aboutUs')->name('about.us');
+Route::get('/contact-us', 'Frontend\FrontendController@contactUs')->name('contact.us');
 
 Auth::routes();
 
+// backend routes
 Route::get('/home', 'HomeController@index')->name('home');
+
+// route group for user management
+Route::prefix('users')->group(function (){
+    Route::get('/view', 'Backend\UserController@view')->name('users.view');
+    Route::get('/add', 'Backend\UserController@add')->name('users.add');
+    Route::post('/store', 'Backend\UserController@store')->name('users.store');
+    Route::get('/edit/{id}', 'Backend\UserController@edit')->name('users.edit');
+    Route::post('/update/{id}', 'Backend\UserController@update')->name('users.update');
+    Route::get('/delete/{id}', 'Backend\UserController@delete')->name('users.delete');
+});
+
+// route group for profiles management
+Route::prefix('profiles')->group(function (){
+    Route::get('/view', 'Backend\ProfileController@view')->name('profiles.view');
+    Route::get('/edit', 'Backend\ProfileController@edit')->name('profiles.edit');
+    Route::post('/update', 'Backend\ProfileController@update')->name('profiles.update');    
+    Route::get('/password/view', 'Backend\ProfileController@passwordView')->name('profiles.password.view');
+    Route::post('/password/update', 'Backend\ProfileController@passwordUpdate')->name('profiles.password.update'); 
+});
